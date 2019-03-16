f20fb21e4fd929dca98ea0197fbbd558b74d43ff
nanigasi <nanigasi.py@gmail.com>
2019-03-16 16:19:13 +0900

    画像のリサイズ(#6)

---
+++ script/resize_image.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+image = Image.open("image/megane.jpg")
+
+src = image.resize((16,16))
+src.save("image/megane_resize16.png","PNG")
+
+src = image.resize((256,256))
+src.save("image/megane_resize256.png","PNG")
